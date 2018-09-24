6b7992d86952d07c9321fe9aadabd497ec8c073a
falakh <zulfikarfahmi38@gmail.com>
2018-09-24 10:57:35 +0700

    initial commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Text highschore;
+	// Use this for initialization
+	void Start () {
+        highschore.text = "High Score : "+PlayerPrefs.GetInt("highScore").ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public  void goPlay(int index)
+    {
+        SceneManager.LoadSceneAsync(index);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
